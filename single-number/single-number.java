8c929e817d7fceab12686d2371b2661fe655ab18
Poojitha Ravuri <58463372+poojitha2002@users.noreply.github.com>
2021-10-16 19:05:58 +0530

    Time: 2 ms (56.49%), Space: 47.8 MB (15.98%) - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+          
+        int xor=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            xor=xor^nums[i];
+        }
+        return xor;
+        
+       
+    }
+}
